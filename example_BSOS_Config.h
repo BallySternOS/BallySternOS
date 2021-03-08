274a5ff708e8f3542bce405b0b1e8b96775f76d9
Dick Hamill <36781010+MrEkted@users.noreply.github.com>
2021-03-07 19:12:01 -0500

    Update example_BSOS_Config.h

--- example_BSOS_Config.h
+++ example_BSOS_Config.h
@@ -3,7 +3,7 @@
 
     I, Dick Hamill, the author of this program disclaim all copyright
     in order to make this program freely available in perpetuity to
-    anyone who would like to use it. Dick Hamill, 6/1/2020
+    anyone who would like to use it. Dick Hamill, 12/1/2020
 
     BallySternOS is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -26,11 +26,43 @@
 
 ***/
 
+// Hardware Rev 1 generally uses an Arduino Nano & (option) 74125
+// Hardware Rev 2 uses an Arduino Nano, a 74155, and a 74240
+#define BALLY_STERN_OS_HARDWARE_REV   1
+
 //#define BALLY_STERN_OS_USE_DIP_SWITCHES 
 //#define BALLY_STERN_OS_USE_SQUAWK_AND_TALK
 //#define BALLY_STERN_OS_USE_SB100
+//#define BALLY_STERN_OS_USE_SB300
+//#define USE_WAV_TRIGGER
+//#define USE_WAV_TRIGGER_1p3
 //#define BALLY_STERN_OS_USE_AUX_LAMPS
 //#define BALLY_STERN_OS_USE_7_DIGIT_DISPLAYS
+//#define BALLY_STERN_OS_DIMMABLE_DISPLAYS
+#define BALLY_STERN_OS_SOFTWARE_DISPLAY_INTERRUPT
+
+// Fast boards might need a slower lamp strobe
+//#define BSOS_SLOW_DOWN_LAMP_STROBE
+
+// Depending on the number of digits, the BALLY_STERN_OS_SOFTWARE_DISPLAY_INTERRUPT_INTERVAL
+// can be adjusted in order to change the refresh rate of the displays.
+// The original -17 / MPU-100 boards ran at 320 Hz 
+// The Alltek runs the displays at 440 Hz (probably so 7-digit displays won't flicker)
+// The value below is calculated with this formula:
+//       Value = (interval in ms) * (16*10^6) / (1*1024) - 1 
+//          (must be <65536)
+// Choose one of these values (or do whatever)
+//  Value         Frequency 
+//  48            318.8 Hz
+//  47            325.5 Hz
+//  46            332.4 Hz increments   (I use this for 6-digits displays)
+//  45            339.6 Hz
+//  40            381 Hz
+//  35            434 Hz     (This would probably be good for 7-digit displays)
+//  34            446.4 Hz      
+#define BALLY_STERN_OS_SOFTWARE_DISPLAY_INTERRUPT_INTERVAL  48  
+
+
 
 #ifdef BALLY_STERN_OS_USE_7_DIGIT_DISPLAYS
 #define BALLY_STERN_OS_MAX_DISPLAY_SCORE  9999999
@@ -41,7 +73,6 @@
 #define BALLY_STERN_OS_NUM_DIGITS         6
 #define BALLY_STERN_OS_ALL_DIGITS_MASK    0x3F
 #endif
-
 
 #ifdef BALLY_STERN_OS_USE_AUX_LAMPS
 #define BSOS_NUM_LAMP_BITS 22
@@ -63,8 +94,6 @@
 // So, set this to (0.000060) / (1/Clock Frequency)
 #define BSOS_NUM_LAMP_LOOPS   30
 
-// Fast boards might need a slower lamp strobe
-#define BSOS_SLOW_DOWN_LAMP_STROBE  0
 
 #define BSOS_CREDITS_EEPROM_BYTE          5
 #define BSOS_HIGHSCORE_EEPROM_START_BYTE  1
