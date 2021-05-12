99fb8909558ff7080cdf50fcc5090c0e9d748d2d
Dick Hamill <36781010+MrEkted@users.noreply.github.com>
2021-05-12 14:51:45 -0400

    Updated for Rev 2 hardware and MPU-200

--- MachineDiagnostics/MachineDiagnostics.ino
+++ MachineDiagnostics/MachineDiagnostics.ino
@@ -1,5 +1,9 @@
+#define BSOS_SWITCH_DELAY_IN_MICROSECONDS         250
+#define BSOS_TIMING_LOOP_PADDING_IN_MICROSECONDS  50
+#define BALLY_STERN_OS_HARDWARE_REV   1
 
 
+#if (BALLY_STERN_OS_HARDWARE_REV==1)
 #define ADDRESS_U10_A           0x14
 #define ADDRESS_U10_A_CONTROL   0x15
 #define ADDRESS_U10_B           0x16
@@ -8,45 +12,78 @@
 #define ADDRESS_U11_A_CONTROL   0x19
 #define ADDRESS_U11_B           0x1A
 #define ADDRESS_U11_B_CONTROL   0x1B
+#define ADDRESS_SB100           0x10
+
+#elif (BALLY_STERN_OS_HARDWARE_REV==2)
+#define ADDRESS_U10_A           0x00
+#define ADDRESS_U10_A_CONTROL   0x01
+#define ADDRESS_U10_B           0x02
+#define ADDRESS_U10_B_CONTROL   0x03
+#define ADDRESS_U11_A           0x08
+#define ADDRESS_U11_A_CONTROL   0x09
+#define ADDRESS_U11_B           0x0A
+#define ADDRESS_U11_B_CONTROL   0x0B
+#define ADDRESS_SB100           0x10
+#define ADDRESS_SB100_CHIMES    0x18
+#define ADDRESS_SB300_SQUARE_WAVES  0x10
+#define ADDRESS_SB300_ANALOG        0x18
+
+#endif 
+
+
 
 void BSOS_DataWrite(int address, byte data) {
-  
+
+  byte newPortB, newPortD;
+  newPortB = (PORTB&0xE0) | (data>>3);
+  newPortD = (PORTD&0x1F) | ((data&0x07)<<5);
+  newPortD = newPortD & 0xF7;
+
+  // Wait while clock is low
+//  while(!(PIND & 0x10));
+
+  // Wait while clock is high
+  while((PIND & 0x10));
+
+  // Set VMA OFF
+//  PORTC = PORTC & 0xDF;
+
+  // Set up address lines
+  PORTC = (PORTC & 0xC0) | address;
+
   // Set data pins to output
   // Make pins 5-7 output (and pin 3 for R/W)
   DDRD = DDRD | 0xE8;
   // Make pins 8-12 output
   DDRB = DDRB | 0x1F;
 
+  // Wait while clock is low
+  while(!(PIND & 0x10));
+
+  // Wait while clock is high
+  while((PIND & 0x10));
+
   // Set R/W to LOW
   PORTD = (PORTD & 0xF7);
+  
+  // Set VMA ON
+  PORTC = PORTC | 0x20;
 
   // Put data on pins
   // Put lower three bits on 5-7
-  PORTD = (PORTD&0x1F) | ((data&0x07)<<5);
+  PORTD = newPortD;
   // Put upper five bits on 8-12
-  PORTB = (PORTB&0xE0) | (data>>3);
+  PORTB = newPortB;
 
-  // Set up address lines
-  PORTC = (PORTC & 0xE0) | address;
-
-  // Wait for a falling edge of the clock
-  while((PIND & 0x10));
-
-  // Pulse VMA over one clock cycle
-  // Set VMA ON
-  PORTC = PORTC | 0x20;
-  
   // Wait while clock is low
   while(!(PIND & 0x10));
+
   // Wait while clock is high
-// Doesn't seem to help --  while((PIND & 0x10));
+  while((PIND & 0x10));
 
-  // Set VMA OFF
-  PORTC = PORTC & 0xDF;
+  // Wait while clock is low
+//  while(!(PIND & 0x10));
 
-  // Unset address lines
-  PORTC = PORTC & 0xE0;
-  
   // Set R/W back to HIGH
   PORTD = (PORTD | 0x08);
 
@@ -55,56 +92,117 @@ void BSOS_DataWrite(int address, byte data) {
   DDRD = DDRD & 0x1F;
   // Make pins 8-12 input
   DDRB = DDRB & 0xE0;
+//  PORTB |= 0x1F;
+//  PORTD |= 0xE0;
+  
+  // Turn off address lines so we don't clear interrupts
+  PORTC = (PORTC & 0xE0);
+#if (BALLY_STERN_OS_HARDWARE_REV==1)
+  // Leave A7 on
+  PORTC |= 0x10;
+#endif
 }
 
 
-
 byte BSOS_DataRead(int address) {
-  
-  // Set data pins to input
-  // Make pins 5-7 input
-  DDRD = DDRD & 0x1F;
-  // Make pins 8-12 input
-  DDRB = DDRB & 0xE0;
 
-  // Set R/W to HIGH
-  DDRD = DDRD | 0x08;
-  PORTD = (PORTD | 0x08);
+  // Wait while clock is low
+  while(!(PIND & 0x10));
+
+  // Wait while the clock is high
+  while((PIND & 0x10));
 
   // Set up address lines
   PORTC = (PORTC & 0xE0) | address;
 
-  // Wait for a falling edge of the clock
-  while((PIND & 0x10));
+  // Wait while clock is low
+  while(!(PIND & 0x10));
 
-  // Pulse VMA over one clock cycle
-  // Set VMA ON
-  PORTC = PORTC | 0x20;
+  // Wait while the clock is high
+  while((PIND & 0x10));
   
   // Wait while clock is low
   while(!(PIND & 0x10));
 
   byte inputData = (PIND>>5) | (PINB<<3);
 
-  // Set VMA OFF
-  PORTC = PORTC & 0xDF;
-
-  // Wait for a falling edge of the clock
-// Doesn't seem to help  while((PIND & 0x10));
-
-  // Set R/W to LOW
-  PORTD = (PORTD & 0xF7);
-
-  // Clear address lines
+  // Turn off address lines so we don't clear interrupts
   PORTC = (PORTC & 0xE0);
+#if (BALLY_STERN_OS_HARDWARE_REV==1)
+  // Leave A7 on
+  PORTC |= 0x10;
+#endif
 
   return inputData;
 }
 
 
 
+
+boolean TestControlRegister(int controlRegisterAddress, byte value) {
+  BSOS_DataWrite(controlRegisterAddress, value);
+  byte returnVal = BSOS_DataRead(controlRegisterAddress);
+  char buf[100];
+  if ( (returnVal&0x3F)!=value ) {
+    // report failure
+    sprintf(buf, "    FAIL: Wrote 0x%02X, got back 0x%02X (masked to 0x%02X)\n", value, returnVal, (returnVal&0x3F));
+    Serial.write(buf);
+    return false;
+  }
+  return true;
+}
+
+boolean TestPort(int portAddress) {
+  char buf[100];
+  boolean testPassed = true;
+  for (int count=0; count<256; count++) {
+    BSOS_DataWrite(portAddress, count & 0xFF);
+    delayMicroseconds(40); // This is a loop of 3 in the original code
+    byte returnValue = BSOS_DataRead(portAddress);
+    if (returnValue!=count) {
+      sprintf(buf, "    FAIL: Wrote 0x%02X, got back 0x%02X\n", count, returnValue);
+      Serial.write(buf);
+      testPassed = false;
+    }
+  }
+  return testPassed;
+}
+
+boolean TestControlRegisterAndPort(int controlRegisterAddress, int portAddress, const char *portName) {
+  boolean passed = true;
+  Serial.write("Testing port ");
+  Serial.write(portName);
+  Serial.write("\n");
+  Serial.write("  Control Register & DDR\n");
+  passed &= TestControlRegister(controlRegisterAddress, 0x31);
+  passed &= TestControlRegister(controlRegisterAddress, 0x39);
+  passed &= TestPort(portAddress);
+  Serial.write("  Control Register & Port\n");
+  passed &= TestControlRegister(controlRegisterAddress, 0x35);
+  passed &= TestControlRegister(controlRegisterAddress, 0x3D);
+  passed &= TestPort(portAddress);
+  if (passed) {
+    Serial.write("Port ");
+    Serial.write(portName);
+    Serial.write(" PASSED\n");
+  }
+
+  return passed;
+}
+
+
 boolean TestPIAChips() {
 
+  boolean testPassed = true;
+
+  testPassed &= TestControlRegisterAndPort(ADDRESS_U10_A_CONTROL, ADDRESS_U10_A, "U10A");
+  testPassed &= TestControlRegisterAndPort(ADDRESS_U10_B_CONTROL, ADDRESS_U10_B, "U10B");
+  testPassed &= TestControlRegisterAndPort(ADDRESS_U11_A_CONTROL, ADDRESS_U11_A, "U11A");
+  testPassed &= TestControlRegisterAndPort(ADDRESS_U11_B_CONTROL, ADDRESS_U11_B, "U11B");
+
+  if (!testPassed) return true;
+  return false;
+/*
   boolean anyRegisterFailed = false;
   boolean chipPassed = true;
   byte testVal; 
@@ -114,7 +212,6 @@ boolean TestPIAChips() {
 
   for (int count=0; count<256; count++) {
     BSOS_DataWrite(ADDRESS_U10_A_CONTROL, count);
-    BSOS_DataRead(0);
     testVal = (BSOS_DataRead(ADDRESS_U10_A_CONTROL))&0x3F;
     char buf[128];
     if (testVal!=(count&0x3F)) {
@@ -133,7 +230,6 @@ boolean TestPIAChips() {
   Serial.write("  Testing U10B Control Register\n");
   for (int count=0; count<256; count++) {
     BSOS_DataWrite(ADDRESS_U10_B_CONTROL, count);
-    BSOS_DataRead(0);
     testVal = (BSOS_DataRead(ADDRESS_U10_B_CONTROL))&0x3F;
     char buf[128];
     if (testVal!=(count&0x3F)) {
@@ -152,7 +248,6 @@ boolean TestPIAChips() {
   Serial.write("  Testing U11A Control Register\n");
   for (int count=0; count<256; count++) {
     BSOS_DataWrite(ADDRESS_U11_A_CONTROL, count);
-    BSOS_DataRead(0);
     testVal = (BSOS_DataRead(ADDRESS_U11_A_CONTROL))&0x3F;
     char buf[128];
     if (testVal!=(count&0x3F)) {
@@ -171,7 +266,6 @@ boolean TestPIAChips() {
   Serial.write("  Testing U11B Control Register\n");
   for (int count=0; count<256; count++) {
     BSOS_DataWrite(ADDRESS_U11_B_CONTROL, count);
-    BSOS_DataRead(0);
     testVal = (BSOS_DataRead(ADDRESS_U11_B_CONTROL))&0x3F;
     char buf[128];
     if (testVal!=(count&0x3F)) {
@@ -187,6 +281,7 @@ boolean TestPIAChips() {
   else Serial.write("    The U11B control register passed\n");
 
   return anyRegisterFailed;
+*/
 }
 
 
@@ -198,12 +293,14 @@ void InitializeU10PIA() {
   // CB2 - lamp strobe 1
   // PA0-7 - output for switch bank, lamps, and BCD
   // PB0-7 - switch returns
-  
+  byte newCRVal;
+
   BSOS_DataWrite(ADDRESS_U10_A_CONTROL, 0x38);
   // Set up U10A as output
   BSOS_DataWrite(ADDRESS_U10_A, 0xFF);
   // Set bit 3 to write data
-  BSOS_DataWrite(ADDRESS_U10_A_CONTROL, BSOS_DataRead(ADDRESS_U10_A_CONTROL)|0x04);
+  newCRVal = BSOS_DataRead(ADDRESS_U10_A_CONTROL)|0x04;
+  BSOS_DataWrite(ADDRESS_U10_A_CONTROL, newCRVal);
   // Store F0 in U10A Output
   BSOS_DataWrite(ADDRESS_U10_A, 0xF0);
   
@@ -211,74 +308,72 @@ void InitializeU10PIA() {
   // Set up U10B as input
   BSOS_DataWrite(ADDRESS_U10_B, 0x00);
   // Set bit 3 so future reads will read data
-  BSOS_DataWrite(ADDRESS_U10_B_CONTROL, BSOS_DataRead(ADDRESS_U10_B_CONTROL)|0x04);
+  newCRVal = BSOS_DataRead(ADDRESS_U10_B_CONTROL)|0x04;
+  BSOS_DataWrite(ADDRESS_U10_B_CONTROL, newCRVal);
+
 
 }
 
 
 byte DipSwitches[4];
-int SwitchDelayNumLoops = 80;
-int LampDelayNumLoops = 60;
 
-void WaitOneClockCycle() {
-  // Wait while clock is low
-  while(!(PIND & 0x10));
-
-  // Wait for a falling edge of the clock
-  while((PIND & 0x10));
+void WaitClockCycles(int numCycles) {
+  for (int count=0; count<numCycles; count++) {
+    // Wait while clock is low
+    while(!(PIND & 0x10));
+  
+    // Wait for a falling edge of the clock
+    while((PIND & 0x10));
+  }
 }
 
 
 void ReadDipSwitches() {
   byte backupU10A = BSOS_DataRead(ADDRESS_U10_A);
   byte backupU10BControl = BSOS_DataRead(ADDRESS_U10_B_CONTROL);
+  BSOS_DataWrite(ADDRESS_U10_A_CONTROL, 0x3C);
+  BSOS_DataWrite(ADDRESS_U10_A, 0x00);
+  BSOS_DataWrite(ADDRESS_U10_B_CONTROL, 0x33);
+  BSOS_DataWrite(ADDRESS_U10_B, 0x00);
+  BSOS_DataWrite(ADDRESS_U10_B_CONTROL, 0x37);
   delay(1);
 
   // Turn on Switch strobe 5 & Read Switches
   BSOS_DataWrite(ADDRESS_U10_A, 0x20);
-  BSOS_DataRead(0);
-  BSOS_DataWrite(ADDRESS_U10_B_CONTROL, backupU10BControl & 0xF7);
-  BSOS_DataRead(0);
   // Wait for switch capacitors to charge
-  for (int count=0; count<SwitchDelayNumLoops; count++) WaitOneClockCycle();
+  delayMicroseconds(BSOS_SWITCH_DELAY_IN_MICROSECONDS);
   DipSwitches[0] = BSOS_DataRead(ADDRESS_U10_B);
-  delay(1);
+  BSOS_DataWrite(ADDRESS_U10_A, 0x00);
+  delayMicroseconds(BSOS_TIMING_LOOP_PADDING_IN_MICROSECONDS);
 
   // Turn on Switch strobe 6 & Read Switches
   BSOS_DataWrite(ADDRESS_U10_A, 0x40);
-  BSOS_DataRead(0);
-  BSOS_DataWrite(ADDRESS_U10_B_CONTROL, backupU10BControl & 0xF7);
-  BSOS_DataRead(0);
   // Wait for switch capacitors to charge
-  for (int count=0; count<SwitchDelayNumLoops; count++) WaitOneClockCycle();
+  delayMicroseconds(BSOS_SWITCH_DELAY_IN_MICROSECONDS);
   DipSwitches[1] = BSOS_DataRead(ADDRESS_U10_B);
-  delay(1);
+  BSOS_DataWrite(ADDRESS_U10_A, 0x00);
+  delayMicroseconds(BSOS_TIMING_LOOP_PADDING_IN_MICROSECONDS);
+
+  // Read from a phony address to wake up the monitor
+  BSOS_DataRead(ADDRESS_U10_B_CONTROL|ADDRESS_U11_B_CONTROL);
 
   // Turn on Switch strobe 7 & Read Switches
   BSOS_DataWrite(ADDRESS_U10_A, 0x80);
-  BSOS_DataRead(0);
-  BSOS_DataWrite(ADDRESS_U10_B_CONTROL, backupU10BControl & 0xF7);
-  BSOS_DataRead(0);
   // Wait for switch capacitors to charge
-  for (int count=0; count<SwitchDelayNumLoops; count++) WaitOneClockCycle();
+  delayMicroseconds(BSOS_SWITCH_DELAY_IN_MICROSECONDS);
   DipSwitches[2] = BSOS_DataRead(ADDRESS_U10_B);
-  delay(1);
+  BSOS_DataWrite(ADDRESS_U10_A, 0x00);
+  delayMicroseconds(BSOS_TIMING_LOOP_PADDING_IN_MICROSECONDS);
 
   // Turn on U10 CB2 (strobe 8) and read switches
   BSOS_DataWrite(ADDRESS_U10_A, 0x00);
-  BSOS_DataRead(0);
   BSOS_DataWrite(ADDRESS_U10_B_CONTROL, backupU10BControl | 0x08);
-  BSOS_DataRead(0);
   // Wait for switch capacitors to charge
-  for (int count=0; count<SwitchDelayNumLoops; count++) WaitOneClockCycle();
+  delayMicroseconds(BSOS_SWITCH_DELAY_IN_MICROSECONDS);
   DipSwitches[3] = BSOS_DataRead(ADDRESS_U10_B);
-  delay(1);
 
   BSOS_DataWrite(ADDRESS_U10_B_CONTROL, backupU10BControl);
-  BSOS_DataRead(0);
   BSOS_DataWrite(ADDRESS_U10_A, backupU10A);
-  BSOS_DataRead(0);
-  delay(1);
 }
 
 
@@ -289,12 +384,14 @@ void InitializeU11PIA() {
   // CB2 - solenoid bank select
   // PA0-7 - display digit enable
   // PB0-7 - solenoid data
+  byte newCRVal;
 
   BSOS_DataWrite(ADDRESS_U11_A_CONTROL, 0x31);
   // Set up U11A as output
   BSOS_DataWrite(ADDRESS_U11_A, 0xFF);
   // Set bit 3 to write data
-  BSOS_DataWrite(ADDRESS_U11_A_CONTROL, BSOS_DataRead(ADDRESS_U11_A_CONTROL)|0x04);
+  newCRVal = BSOS_DataRead(ADDRESS_U11_A_CONTROL)|0x04;
+  BSOS_DataWrite(ADDRESS_U11_A_CONTROL, newCRVal);
   // Store 00 in U11A Output
   BSOS_DataWrite(ADDRESS_U11_A, 0x00);
   
@@ -302,31 +399,31 @@ void InitializeU11PIA() {
   // Set up U11B as output
   BSOS_DataWrite(ADDRESS_U11_B, 0xFF);
   // Set bit 3 so future reads will read data
-  BSOS_DataWrite(ADDRESS_U11_B_CONTROL, BSOS_DataRead(ADDRESS_U11_B_CONTROL)|0x04);
+  newCRVal = BSOS_DataRead(ADDRESS_U11_B_CONTROL)|0x04;
+  BSOS_DataWrite(ADDRESS_U11_B_CONTROL, newCRVal);
   // Store 9F in U11B Output
   BSOS_DataWrite(ADDRESS_U11_B, 0x9F);
 //  CurrentSolenoidByte = 0x9F;
-  
+
 }
 
 boolean SetupPIAChips() {
   boolean failed = false;
-
+  
   InitializeU10PIA();
   InitializeU11PIA();
+  ReadDipSwitches();
 
   Serial.write("Testing the clock cycle (if this hangs here, the conncection to clock is faulty)\n");
   unsigned long startTime = micros();
-  for (int count=0; count<10000; count++) WaitOneClockCycle();
+  WaitClockCycles(10000);
   unsigned long totalTime = micros() - startTime;
   char buf[128];
   sprintf(buf, "It took %lu microseconds for 10000 clock cycles\n", totalTime);
   Serial.write(buf);
   unsigned long frequency = 10000000 / totalTime;
-  sprintf(buf, "Clock frequency (very) approximately = %d kHz\n", frequency);
+  sprintf(buf, "Clock frequency (very) approximately = %lu kHz\n", frequency);
   Serial.write(buf);
-
-  ReadDipSwitches();
 
   for (int count=0; count<4; count++) {
     char buf[128];
@@ -349,7 +446,6 @@ void InterruptServiceRoutine() {
   byte u10AControl = BSOS_DataRead(ADDRESS_U10_A_CONTROL);
   if (u10AControl & 0x80) {
     // self test switch
-//    if (BSOS_DataRead(ADDRESS_U10_A_CONTROL) & 0x80) PushToSwitchStack(SW_SELF_TEST_SWITCH);
     BSOS_DataRead(ADDRESS_U10_A);
   }
 
@@ -359,18 +455,12 @@ void InterruptServiceRoutine() {
   // If the interrupt bit on the display interrupt is on, do the display refresh
   if (u11AControl & 0x80) {
     BSOS_DataRead(ADDRESS_U11_A);
-    BSOS_DataRead(0);
-    BSOS_DataRead(ADDRESS_U11_A);
-    BSOS_DataRead(0);
     numU11AInterrupts += 1;
   }
 
   // If the IRQ bit of U10BControl is set, do the Zero-crossing interrupt handler
   if (u10BControl & 0x80) {
     BSOS_DataRead(ADDRESS_U10_B);
-    BSOS_DataRead(0);
-    BSOS_DataRead(ADDRESS_U10_B);
-    BSOS_DataRead(0);
     numU10BInterrupts += 1;
   }
 }
@@ -386,15 +476,13 @@ boolean TestInterrupts() {
   numU11AInterrupts = 0;
   // Hook up the interrupt
   attachInterrupt(digitalPinToInterrupt(2), InterruptServiceRoutine, LOW);
-  BSOS_DataRead(0);  // Reset address bus
 
+  // Wait for 5 seconds
   while ((millis()-startTime)<5000) {
-    BSOS_DataRead(0);
   }
 
-    // Hook up the interrupt
+  // Hook up the interrupt
   detachInterrupt(digitalPinToInterrupt(2));
-  BSOS_DataRead(0);  // Reset address bus
 
   char buf[128];
   char floatStr[10];
@@ -461,8 +549,6 @@ void setup() {
 
   Serial.write("Saw no sign of M6800 -- program will continue\n");
 
-
-
   // Arduino A0 = MPU A0
   // Arduino A1 = MPU A1
   // Arduino A2 = MPU A3
@@ -486,34 +572,49 @@ void setup() {
   DDRD = DDRD | 0xE8;
 
   digitalWrite(3, HIGH);  // Set R/W line high (Read)
+  pinMode(A5, OUTPUT);
   digitalWrite(A5, LOW);  // Set VMA line LOW
   digitalWrite(A6, HIGH); // Set
 
   pinMode(2, INPUT);
+  pinMode(13, INPUT_PULLUP);
 
-  // Prep the address bus (all lines zero)
-  BSOS_DataRead(0);
+}
 
 
-  if (TestPIAChips()) {
-    Serial.write("FAILED\n");
-    while (1);
-  }
-  if (SetupPIAChips()) {
-    Serial.write("FAILED\n");
-    while (1);
-  }
-  if (TestInterrupts()) {
-    Serial.write("FAILED\n");
-  }
+void TestLightOn() {
+  byte newCRVal = BSOS_DataRead(ADDRESS_U11_A_CONTROL) | 0x08;
+  BSOS_DataWrite(ADDRESS_U11_A_CONTROL, newCRVal);
+}
 
-  Serial.write("END OF TESTS\n");
+void TestLightOff() {
+  byte newCRVal = BSOS_DataRead(ADDRESS_U11_A_CONTROL) & 0xF7;
+  BSOS_DataWrite(ADDRESS_U11_A_CONTROL, newCRVal);
 }
 
 
 
-
 void loop() {
-  delay(5000);
-  Serial.write("(waiting for reset)\n");
+  if (TestPIAChips()) {
+    Serial.write("FAILED\n");
+  }
+  if (SetupPIAChips()) {
+    Serial.write("FAILED\n");
+  }
+  if (TestInterrupts()) {
+    Serial.write("FAILED\n");
+  }
+  InitializeU10PIA();
+  InitializeU11PIA();
+
+  Serial.write("END OF TESTS\n");  
+  for (byte count=0; count<2; count++) {
+    delay(500);
+    TestLightOn();
+    Serial.write("ON\n");  
+    delay(500);
+    TestLightOff();
+    Serial.write("OFF\n");  
+  }
+  Serial.write("\n\nGoing to try again...\n");
 }
